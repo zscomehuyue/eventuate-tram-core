caac73e7c579fe68bc222b3aa1f186375d70aca3
zscomehuyue <zscomehuyue@126.com>
2020-06-10 17:12:44 +0800

    dd

--- eventuate-tram-consumer-common/src/main/java/io/eventuate/tram/consumer/common/MessageConsumerImplementation.java
+++ eventuate-tram-consumer-common/src/main/java/io/eventuate/tram/consumer/common/MessageConsumerImplementation.java
@@ -6,7 +6,9 @@ import io.eventuate.tram.messaging.consumer.MessageSubscription;
 import java.util.Set;
 
 public interface MessageConsumerImplementation {
-  MessageSubscription subscribe(String subscriberId, Set<String> channels, MessageHandler handler);
-  String getId();
-  void close();
+    MessageSubscription subscribe(String subscriberId, Set<String> channels, MessageHandler handler);
+
+    String getId();
+
+    void close();
 }
--- eventuate-tram-messaging/src/main/java/io/eventuate/tram/messaging/common/Message.java
+++ eventuate-tram-messaging/src/main/java/io/eventuate/tram/messaging/common/Message.java
@@ -5,25 +5,32 @@ import java.util.Optional;
 
 /**
  * A message
+ *
  * @see io.eventuate.tram.messaging.producer.MessageBuilder
  */
 public interface Message {
-  String ID = "ID";
-  String PARTITION_ID = "PARTITION_ID";
-  String DESTINATION = "DESTINATION";
-  String DATE = "DATE";
+    String ID = "ID";
+    String PARTITION_ID = "PARTITION_ID";
+    String DESTINATION = "DESTINATION";
+    String DATE = "DATE";
 
-  String getId();
-  Map<String, String> getHeaders();
-  String getPayload();
+    String getId();
 
-  Optional<String> getHeader(String name);
-  String getRequiredHeader(String name);
+    Map<String, String> getHeaders();
 
-  boolean hasHeader(String name);
+    String getPayload();
 
-  void setPayload(String payload);
-  void setHeaders(Map<String, String> headers);
-  void setHeader(String name, String value);
-  void removeHeader(String key);
+    Optional<String> getHeader(String name);
+
+    String getRequiredHeader(String name);
+
+    boolean hasHeader(String name);
+
+    void setPayload(String payload);
+
+    void setHeaders(Map<String, String> headers);
+
+    void setHeader(String name, String value);
+
+    void removeHeader(String key);
 }
